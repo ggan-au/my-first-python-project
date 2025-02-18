f9293a03b8343a5866e72f39294f751ac27c92bd
ai1002hash <glenganderton@proton.me>
2025-02-18 18:14:37 +1100

    STAGE 2 - PASSED

--- calculator.py
+++ calculator.py
@@ -1,11 +1,25 @@
-prices = {
-'Bubblegum': '$2',
-'Toffee': '$0.2',
-'Ice cream': '$5',
-'Milk chocolate': '$4',
-'Doughnut': '$2.5',
-'Pancake': '$3.2',
+# prices = {
+# 'Bubblegum': '$2',
+# 'Toffee': '$0.2',
+# 'Ice cream': '$5',
+# 'Milk chocolate': '$4',
+# 'Doughnut': '$2.5',
+# 'Pancake': '$3.2',
+# }
+
+# print('Prices:')
+# [print(f'{item}: {price}') for item, price in prices.items()]
+
+earnings = {
+'Bubblegum': '202',
+'Toffee': '118',
+'Ice cream': '2250',
+'Milk chocolate': '1680',
+'Doughnut': '1075',
+'Pancake': '80',
 }
 
-print('Prices:')
-[print(f'{item}: {price}') for item, price in prices.items()]
+print('Earned Amount:')
+[print(f"{item}: ${price}")for item, price in earnings.items()]
+income = sum([int(price) for price in earnings.values()])
+print(f"\nIncome: ${income}")
