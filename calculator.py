6e4a1aa83c7c7641145c913a673b825b79938859
ai1002hash <glenganderton@proton.me>
2025-02-18 17:23:46 +1100

    Stage 1 - PASSED

---
+++ calculator.py
@@ -0,0 +1,12 @@
+prices = {
+'Bubblegum': '$2',
+'Toffee': '$0.2',
+'Ice cream': '$5',
+'Milk chocolate': '$4',
+'Doughnut': '$2.5',
+'Pancake': '$3.2',
+}
+
+print('Prices:')
+for item, price in prices.items():
+    print(f'{item}: {price}')
